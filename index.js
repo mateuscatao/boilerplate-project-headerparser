de2cd0ce70f0824b1e598c7aa362f99de61699b6
mateuscatao <mateuscatao1311@gmail.com>
2023-11-27 14:29:02 -0300

    solution

--- index.js
+++ index.js
@@ -19,10 +19,14 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
+// My solution
+app.get("/api/whoami", (req, res) => {
+  const ipaddress = req.ip;
+  const language = req.headers["accept-language"];
+  const software = req.headers["user-agent"];
+
+  res.json({ ipaddress: ipaddress, language: language, software: software });
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
